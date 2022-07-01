c9daf2c89bcf84df5507cff1dafb1bbb03791abb
Sayan <sayanmondal2098@gmail.com>
2022-07-01 23:51:47 +0530

    array problem 122

--- Solution.java
+++ Solution.java
@@ -1,28 +1,26 @@
+import java.math.BigInteger;
 import java.util.*;
- 
+
+
+// solution idea I : Check the last digit , if its 9 , then increase the new array size by one , other wise copy and increase the last element by one
 public class Solution
 {
     public int maxProfit(int[] prices) {
-        int max_profit = 0, buy = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i]<buy) {
-                buy = prices[i];
-            }
-            else if (max_profit < prices[i] - buy) {
-                max_profit = prices[i] - buy;
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                ans += prices[i] - prices[i - 1];
             }
         }
-        return max_profit;
+        return ans;
     }
  
     /* Driver Function to test other function */
     public static void main(String[] args)
     {
         Solution gfg = new Solution();
-        int [] cardPoints = {9,7,7,9,7,7,9}; int k = 18;
-        System.out.println(gfg.twoSum(cardPoints, k));
-        for (int num : gfg.twoSum(cardPoints, k)) {
-            System.out.print(num + " ");
-        }
+        int [] cardPoints = {9,8,7,6,5,4,3,2,1,0}; int k = 18;
+        gfg.plusOne(cardPoints);
+        
     }
 }
